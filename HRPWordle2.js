4c4ffae127c25d4ffc411eb1094a17ae0b5de549
runyu1413 <79622204+runyu1413@users.noreply.github.com>
2022-04-20 20:19:05 +0800

    Update HRPWordle2.js

--- HRPWordle2.js
+++ HRPWordle2.js
@@ -1058,7 +1058,7 @@ function intialize() {
     let keyboard = [
         ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
         ["A", "S", "D", "F", "G", "H", "J", "K", "L", "Z"],
-        ["Enter", "X", "C", "V", "B", "N", "M", "⌫" ]
+        ["Enter", "X", "C", "V", "B", "N", "M", "Delete" ]
     ]
 
     for (let i = 0; i < keyboard.length; i++) {
@@ -1074,7 +1074,7 @@ function intialize() {
             if (key == "Enter") {
                 keyTile.id = "Enter";
             }
-            else if (key == "⌫") {
+            else if (key == "Delete") {
                 keyTile.id = "Backspace";
             }
             else if ("A" <= key && key <= "Z") {
@@ -1083,7 +1083,7 @@ function intialize() {
 
             keyTile.addEventListener("click", processKey);
 
-            if (key == "Enter" || key == "⌫") {
+            if (key == "Enter" || key == "Delete") {
                 keyTile.classList.add("enter-key-tile");
             } else {
                 keyTile.classList.add("key-tile");
